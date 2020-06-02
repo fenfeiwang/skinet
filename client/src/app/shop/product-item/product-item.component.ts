d0f6de1d35d9fc024eeac432ae51c8b4641e3fef
fenfei wang <fwang327@gatech.edu>
2020-06-02 11:54:19 -0500

    Add Client Shop module

---
+++ client/src/app/shop/product-item/product-item.component.ts
@@ -0,0 +1,21 @@
+import { BasketService } from './../../basket/basket.service';
+import { IProduct } from './../../shared/models/product';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.scss']
+})
+export class ProductItemComponent implements OnInit {
+  @Input() product: IProduct;
+
+  constructor(private basketService: BasketService) { }
+
+  ngOnInit(): void {
+  }
+
+  addItemToBasket(){
+    this.basketService.addItemToBasket(this.product);
+  }
+}
--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -3,12 +3,12 @@ import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
-import { ProdutItemComponent } from './produt-item/produt-item.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 @NgModule({
-  declarations: [ShopComponent, ProdutItemComponent, ProductDetailsComponent],
+  declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
   imports: [
     CommonModule,
     SharedModule,
