48bde02e5feae5f4356d79b22f9ac8bcfc022168
fenfei wang <fwang327@gatech.edu>
2020-06-14 11:59:34 -0500

    Complete Identity on Client

--- client/src/app/core/interceptors/loading.interceptors.ts
+++ client/src/app/core/interceptors/loading.interceptors.ts
@@ -5,11 +5,13 @@ import { Injectable } from '@angular/core';
 import { finalize, delay } from 'rxjs/operators';
 
 @Injectable()
-export class LoadingInterceptor implements HttpInterceptor{
-    constructor(private busyService: BusyService){}
+export class LoadingInterceptor implements HttpInterceptor {
+    constructor(private busyService: BusyService) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        this.busyService.busy();
+        if (!req.url.includes('emailexists')) {
+            this.busyService.busy();
+        }
         return next.handle(req).pipe(
             delay(1000),
             finalize(() => {
